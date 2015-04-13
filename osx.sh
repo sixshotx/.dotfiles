c03d41578da23cee0e1d5dcffc65f06822bd9396
jasoncyu <jason@liketwice.com>
2015-04-13 16:03:46 -0700

    Add amethyst and osx.sh

---
+++ osx.sh
@@ -0,0 +1,5 @@
+brew install caskroom/cask/brew-cask
+brew cask install amethyst
+
+# Totalspaces config
+# http://discuss.binaryage.com/t/please-add-an-option-to-select-the-active-monitor-based-on-the-active-window-not-the-mouse-position/1950
