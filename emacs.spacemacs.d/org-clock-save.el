a5c84a39ba9bdbe7cc824782d96df2a762e4645f
jasoncyu <jason@liketwice.com>
2015-05-03 18:36:37 -0700

    ra;dom

--- emacs.spacemacs.d/org-clock-save.el
+++ emacs.spacemacs.d/org-clock-save.el
@@ -1,3 +1,3 @@
-;; org-persist.el - Jasons-MacBook-Pro-2.local at <2015-05-02 Sat 13:25>
-(setq resume-clock '("/Users/jason/Dropbox/org/life.org" . 16982))
-(setq stored-clock-history '(("/Users/jason/Dropbox/org/twice.org" . 59131) ("/Users/jason/Dropbox/org/life.org" . 16881)))
+;; org-persist.el - Jasons-MacBook-Pro-2.local at <2015-05-03 Sun 17:34>
+(setq resume-clock '("/Users/jason/Dropbox/org/life.org" . 29224))
+(setq stored-clock-history '(("/Users/jason/Dropbox/org/twice.org" . 59131) ("/Users/jason/Dropbox/org/life.org" . 17128) ("/Users/jason/Dropbox/org/life.org" . 16259) ("/Users/jason/Dropbox/org/life.org" . 18645) ("/Users/jason/Dropbox/org/life.org" . 18497) ("/Users/jason/Dropbox/org/life.org" . 18770) ("/Users/jason/Dropbox/org/refile.org" . 15225) ("/Users/jason/Dropbox/org/refile.org" . 14968) ("/Users/jason/Dropbox/org/life.org" . 29125)))
