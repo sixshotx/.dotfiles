586044c92b51bac0f372c2ae2c2613c3a3c620f8
jasoncyu <jason@liketwice.com>
2015-05-11 11:48:38 -0700

    Let the spacemacs repo track spacemacs. I won't be changing stuff in this directory.

--- emacs.spacemacs.d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - Jasons-MacBook-Pro-2.local at <2015-05-05 Tue 23:44>
-(setq stored-clock-history '(("/Users/jason/Dropbox/org/twice.org" . 62724) ("/Users/jason/Dropbox/org/twice.org" . 32455) ("/Users/jason/Dropbox/org/twice.org" . 41085) ("/Users/jason/Dropbox/org/twice.org" . 63910) ("/Users/jason/Dropbox/org/twice.org" . 59051) ("/Users/jason/Dropbox/org/twice.org" . 64304) ("/Users/jason/Dropbox/org/life.org" . 14616)))
