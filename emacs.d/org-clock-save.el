9b98e2f6318d8894e7a9785778913279c98e8feb
jasoncyu <jason@liketwice.com>
2015-05-13 15:22:34 -0700

    Remove old emacs prelude setup

--- emacs.d/org-clock-save.el
+++
@@ -1,3 +0,0 @@
-;; org-persist.el - Jasons-MacBook-Pro-2.local at <2015-04-27 Mon 16:08>
-(setq resume-clock '("/Users/jason/Dropbox/org/life.org" . 15887))
-(setq stored-clock-history '(("/Users/jason/Dropbox/org/twice.org" . 56079) ("/Users/jason/Dropbox/org/twice.org" . 58035) ("/Users/jason/Dropbox/org/life.org" . 15830)))
