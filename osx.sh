5158023755eabfef53b41435e955094fc9d42804
jasoncyu <jason@liketwice.com>
2015-06-19 18:15:50 -0700

    Support Linux

--- osx.sh
+++ osx.sh
@@ -4,13 +4,3 @@ brew cask install amethyst
 
 # Totalspaces config
 # http://discuss.binaryage.com/t/please-add-an-option-to-select-the-active-monitor-based-on-the-active-window-not-the-mouse-position/1950
-
-# common.sh
-# oh-my-zsh
-# https://github.com/robbyrussell/oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# FZF
-# https://github.com/junegunn/fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
