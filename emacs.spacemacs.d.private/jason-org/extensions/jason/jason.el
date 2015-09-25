59278637b1b56a289f9d47b0990a0645cdd4886d
jasoncyu <jason@liketwice.com>
2015-09-25 13:00:05 -0700

    Document TODO keyword specification syntax

--- emacs.spacemacs.d.private/jason-org/extensions/jason/jason.el
+++ emacs.spacemacs.d.private/jason-org/extensions/jason/jason.el
@@ -46,8 +46,21 @@
 
 ; Org mode todo settings
 
+;; Keywords after "|" are terminal
+;; STATE(@/!).
+;; @ means adding a time and prompting user to enter a tnote
+;; ! means only adding a time.
+;; X/Y means "do X when entering a state" and Y means "do Y" iff the target
+;; state does not define X
+;;;
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)") (sequence "WAITING(w@/!)" "|" "CANCELED(c@)" ))))
+      ;; TODO means stuff to do that we haven't started.
+      ;; NEXT means that we've started doing it and might need to finish it.
+      ;; WAITING means that we're waiting on external input before we can
+      ;; continue working on it.
+      ;; CANCELED means we don't want to do it at all
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "|" "CANCELED(c@)" ))))
 ;; Allows changing todo states with S-left and S-right w/o triggering things like
 ;; notes and state triggers.
 (setq org-treat-S-cursor-todo-selection-as-state-change nil)
