445f089a493b8f42c8756b6d7213eeef25fff2f3
jasoncyu <jason@liketwice.com>
2015-11-18 12:49:46 -0800

    Comment out unhelpful js packages

--- emacs.spacemacs.d.private/jason-js/packages.el
+++ emacs.spacemacs.d.private/jason-js/packages.el
@@ -14,11 +14,11 @@
   '(
     ;; package jason-jss go here
     ;; The famous js2-mode that steve yegge loves.
-    js2-mode
+    ;; js2-mode
     ;; A dependency of ac-js2
-    skewer-mode
+    ;; skewer-mode
     ;; Autocomplete for js2
-    ac-js2
+    ;; ac-js2
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -35,19 +35,3 @@ which require an initialization must be listed explicitly in the list.")
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
 ;; https://github.com/jwiegley/use-package
-
-(defun jason-js/init-js2-mode '()
-  "Sets up js2-mode"
-  ;; Install as major mode for js files.
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  ;; Install as minor mode for js linting.
-  ;; Set node as interpreter.
-  (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-  (add-hook 'js-mode-hook 'js2-minor-mode)
-  )
-
-(defun jason-js/init-ac-js2 '()
-  "Sets up ac-js2"
-  ;; Enable js2 autocomplete.
-  (add-hook 'js2-mode-hook 'ac-js2-mode)
-  )
